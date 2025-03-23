ac26202c5136e728b6271dd1d848aba20d8b5540
simonavans <saj.decock@student.avans.nl>
2025-03-23 13:36:49 +0100

    added default ts parsers

--- lua/sdecock/plugin/treesitter.lua
+++ lua/sdecock/plugin/treesitter.lua
@@ -7,7 +7,19 @@ return {
 
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "html", "lua", "markdown", "vim", "vimdoc" },
+				ensure_installed = {
+					"c",
+					"diff",
+					"git_rebase",
+					"gitcommit",
+					"gitignore",
+					"html",
+					"lua",
+					"markdown",
+					"query",
+					"vim",
+					"vimdoc",
+				},
 				-- Autoinstall languages that are not installed
 				auto_install = true,
 				highlight = { enable = true },
